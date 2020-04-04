d81a78f589c147cf8813f46dde67b2c785b00d68
Herman Schaumburg <herman.schaumburg@gmail.com>
2020-04-03 22:54:04 -0500

    Added viewBox attribute to svg to make it adaptive

--- static/js/prices.js
+++ static/js/prices.js
@@ -17,8 +17,11 @@ var chartHeight = svgHeight - margin.top - margin.bottom;
 // Select body, append SVG area to it, and set its dimensions
 var svg = d3.select("#price_plot")
   .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
+  .attr("viewBox" ,"0 0 960 500")
+// Followed instructions under heading SVG embedded inline using the <svg> tag
+// https://tympanus.net/codrops/2014/08/19/making-svgs-responsive-with-css/
+//  .attr("width", svgWidth)
+//  .attr("height", svgHeight);
 
 // Append a group area, then set its margins
 var chartGroup = svg.append("g")
@@ -88,34 +91,34 @@ d3.csv("/static/data/HistPrices.csv").then(function(forceData){
   console.log(error);
 });
 // Call updatePlotly() when a change takes place to the DOM
-d3.selectAll("#selDataset").on("change", updatePlotly);
+//d3.selectAll("#selDataset").on("change", updatePlotly);
 
-// This function is called when a dropdown menu item is selected
-function updatePlotly() {
-  // Use D3 to select the dropdown menu
-  var dropdownMenu = d3.select("#selDataset");
-  // Assign the value of the dropdown menu option to a variable
-  var dataset = dropdownMenu.property("value");
+// // This function is called when a dropdown menu item is selected
+// function updatePlotly() {
+//   // Use D3 to select the dropdown menu
+//   var dropdownMenu = d3.select("#selDataset");
+//   // Assign the value of the dropdown menu option to a variable
+//   var dataset = dropdownMenu.property("value");
 
-  // Initialize x and y arrays
-  var x = [];
-  var y = [];
+//   // Initialize x and y arrays
+//   var x = [];
+//   var y = [];
 
-  if (dataset === 'dataset1') {
-    x = [1, 2, 3, 4, 5];
-    y = [1, 2, 4, 8, 16];
-  }
+//   if (dataset === 'dataset1') {
+//     x = [1, 2, 3, 4, 5];
+//     y = [1, 2, 4, 8, 16];
+//   }
 
-  if (dataset === 'dataset2') {
-    x = [10, 20, 30, 40, 50];
-    y = [1, 10, 100, 1000, 10000];
-  }
+//   if (dataset === 'dataset2') {
+//     x = [10, 20, 30, 40, 50];
+//     y = [1, 10, 100, 1000, 10000];
+//   }
 
-  // Note the extra brackets around 'x' and 'y'
-  Plotly.restyle("plot", "x", [x]);
-  Plotly.restyle("plot", "y", [y]);
+//   // Note the extra brackets around 'x' and 'y'
+//   Plotly.restyle("plot", "x", [x]);
+//   Plotly.restyle("plot", "y", [y]);
 
-  } Plotly.newplot("x", "y");
+//   } Plotly.newplot("x", "y");
 
 
 
